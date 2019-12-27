481b3ff481e7307d90f8cbc38b5d1c6db91073bf
hongkim9815 <hongkim9815@gmail.com>
2019-12-27 21:17:14 +0900

    [V.T01] Add testfile for testing internet, tkinter, python, and Hangeul.

---
+++ test/weather_tk_hangeul.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#-*- coding: utf8 -*-
+
+import requests
+from tkinter import *
+
+
+querylist = ['', '오늘오전', '오늘오후날씨', '내일오전', '내일오후']
+
+db = []
+for i in range(len(querylist)):
+    html = requests.get('https://search.naver.com/search.naver?query=전국'+querylist[i]+'날씨')
+    html.encoding = 'utf-8'
+    html = html.text
+    daejeon_class_name = 'ct006005'
+    daejeon_index = html.index('w_box '+ daejeon_class_name)
+    state_index = html.index('>', daejeon_index + 5 + len(daejeon_class_name) + 3) + 1
+    daejeon_state = html[state_index : html.index('</', state_index)]
+    daejeon_temperature = html[html.index('dsc', state_index) + 5 : html.index('</a>', state_index) - 8]
+    db.append((daejeon_state, daejeon_temperature))
+
+tkmain = Tk()
+tkmain.geometry('480x720')
+for i in range(len(querylist)):
+    lbl = Label(tkmain, text=db[i][0] + ' / ' + db[i][1])
+    lbl.grid(column=0, row=i)
+lbl = Label(tkmain, text='asdf')
+lbl.grid(column = 10, row=100)
+tkmain.mainloop()
