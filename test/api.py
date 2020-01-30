29650d8f897fbea8ab76d255bc0d3b7eba7bd129
Kihong Kim <hongkim9815@gmail.com>
2020-01-30 21:30:04 +0900

    [v.T13] add api test file; add some sources related to badge;

---
+++ test/api.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Title   api.py
+Author  Kihong Kim (Undergraduate Student, School of Computing, KAIST)
+Made    30-Jan-2020
+Comment This program is test for compatibility with API of framework on magicoding.io.
+"""
+
+import requests
+import json
+
+
+if __name__ == "__main__":
+    try:
+        result = requests.get("http://ec2-13-209-200-6.ap-northeast-2.compute.amazonaws.com"
+                              + "/api/Gateway/Wands/%02d/User" % (1))
+    except requests.exceptions.ConnectionError:
+        print("requests: requests.get() got an exception...")
+        exit(0)
+
+    result = json.loads(result.text)
+
+    if result['result_state'] is not 1:
+        print("requests: requests.get() got an exception...")
+        exit(0)
+
+    result = result['result']
+    print(result['today_mission']['mission_order'])
+    print(result['today_mission']['mission_title'])
+    for rt in result['unit_list']:
+        for rtt in rt['mission_list']:
+            if rtt['mission_order'] == result['today_mission']['mission_order']:
+                print(rtt)
+                break
+
+    for rt in result['badge_info']:
+        print(rt['badge_url'])
+
+
+
+
+
